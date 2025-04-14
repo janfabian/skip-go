c3213cc976e877a3305ea5a4af7355ffc656205b
janfabian <mail@janfabian.cz>
2025-04-14 15:00:22 +0200

    webcomponent functions

--- packages/widget/src/web-component.tsx
+++ packages/widget/src/web-component.tsx
@@ -2,6 +2,8 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import toWebComponent from "@r2wc/react-to-web-component";
 import { Widget } from "./widget/Widget";
+import { SignerGetters, SkipClientOptions } from "@skip-go/client";
+import { Callbacks } from "./state/callbacks";
 
 type WebComponentProps = {
   container: {
@@ -23,7 +25,14 @@ const camelize = (inputString: string) => {
   return inputString.replace(/-./g, (x) => x[1].toUpperCase());
 };
 
-const WidgetWithProvider = (props: WebComponentProps) => {
+type FuncProps = Pick<
+  NonNullable<SkipClientOptions["endpointOptions"]>,
+  "getRestEndpointForChain" | "getRpcEndpointForChain"
+> &
+  SignerGetters &
+  Callbacks;
+
+const WidgetWithProvider = (props: WebComponentProps & FuncProps) => {
   const parsedProps = Array.from(props.container.attributes).map(({ name, value }) => {
     return { key: name, value };
   });
@@ -35,15 +44,48 @@ const WidgetWithProvider = (props: WebComponentProps) => {
       accumulator[camelize(key)] = isJsonString(value) ? JSON.parse(value) : value;
       return accumulator;
     },
-    {} as Record<string, string>,
+    {} as Record<string, object | string>,
   );
 
-  return <Widget {...realProps} />;
+  const { container, ...funcProps } = props;
+
+  const endpointOptions = (realProps.endpointOptions || {}) as object;
+
+  if (funcProps.getRestEndpointForChain) {
+    realProps.endpointOptions = {
+      ...endpointOptions,
+      getRestEndpointForChain: funcProps.getRestEndpointForChain,
+    };
+  }
+
+  if (funcProps.getRpcEndpointForChain) {
+    realProps.endpointOptions = {
+      ...endpointOptions,
+      getRpcEndpointForChain: funcProps.getRpcEndpointForChain,
+    };
+  }
+
+  const { getRestEndpointForChain, getRpcEndpointForChain, ...rootFuncProps } = funcProps;
+
+  return <Widget {...realProps} {...rootFuncProps} />;
 };
 
 const WEB_COMPONENT_NAME = "skip-widget";
 
-const WebComponent = toWebComponent(WidgetWithProvider);
+const WebComponent = toWebComponent(WidgetWithProvider, {
+  props: {
+    getCosmosSigner: "function",
+    getEVMSigner: "function",
+    getSVMSigner: "function",
+    onWalletConnected: "function",
+    onWalletDisconnected: "function",
+    onTransactionBroadcasted: "function",
+    onTransactionComplete: "function",
+    onTransactionFailed: "function",
+    getRpcEndpointForChain: "function",
+    getRestEndpointForChain: "function",
+  },
+});
 
 function initializeSkipWidget() {
   if (!customElements.get(WEB_COMPONENT_NAME)) {
